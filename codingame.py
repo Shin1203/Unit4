67633445d3fc25c7216c60cf3bdbd0194cd4310e
Shin M <54606968+Shin1203@users.noreply.github.com>
2020-05-22 05:13:59 +0900

    Create codingame.py

---
+++ codingame.py
@@ -0,0 +1,36 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+def distancecalc(longitudeA, latitudeA, longitudeB, latitudeB):
+    longitudeA = math.radians(longitudeA)
+    longitudeB = math.radians(longitudeB)
+    latitudeA = math.radians(latitudeA)
+    latitudeB = math.radians(latitudeB)
+    x = (longitudeB - longitudeA) * math.cos((latitudeA + latitudeB) / 2)
+    y = latitudeB - latitudeA
+    return math.sqrt(x * x + y * y)*6371
+longitudeA =  float(input().replace(',', '.'))
+latitudeA = float(input().replace(',', '.'))
+n = int(input())
+
+for i in range(n):
+    defib = input().split(";")
+
+
+    # longitude and lattitude variables, also replace "," with "."
+    longitudeB = float(defib[4].replace(',', '.'))
+    latitudeB = float(defib[5].replace(',', '.'))
+    # The math
+    distancem = distancecalc(longitudeA, latitudeA, longitudeB, latitudeB)
+    smallestD = 0
+    if distancem < smallestD or smallestD == 0:
+        smallestD = distancem
+        location = defib[1]
+
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr)
+
+print(location)
